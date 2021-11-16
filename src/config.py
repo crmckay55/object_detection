9193aee518322dc0387928af5debf07661191835
crmckay55 <crmckay55@gmail.com>
2021-11-16 14:16:15 -0700

    final commit of project

--- src/config.py
+++ src/config.py
@@ -20,8 +20,8 @@ COMPUTATION_DEVICE = 'cuda'
 DEVICE = torch.device(COMPUTATION_DEVICE)
 BATCH_SIZE = 9      # increase / decrease according to GPU memory - 9 works for GeForce 1070, 8GB RAM
 RESIZE_TO = 640     # resize the image for training and transforms - TODO: how big can we go?  is bigger better?
-NUM_EPOCHS = 1000    # number of epochs to train for
-# TODO: how do epochs relate to training quality (output graphs) - other parameters?
+NUM_EPOCHS = 100    # number of epochs to train for - 100 for testing, 1000 for final??
+
 NUM_CLASSES = 2
 
 LEARNING_RATE_START = 0.0002     # default 0.001
